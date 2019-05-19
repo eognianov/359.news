a7bf46a8d32bcd97c31c69c58346fcb9e3489ebf
eognianov <emilian_89@abv.bg>
2019-05-19 23:45:04 +0300

    front end

--- src/FrontEnd/assets/js/site.js
+++ src/FrontEnd/assets/js/site.js
@@ -1,15 +1,17 @@
-$notifications = $('.toast__close i').on('click', function (event) {
+(function ($) {
+   
+    $notifications = $('.toast__close i').on('click', cookieHandler);
     // cookie
-    event.stopPropagation();
-    $notification = $(this.parentNode.parentNode);
-    console.log($notification.siblings().filter(".hidden").length);
-    console.log($notification.siblings().length);
-    if ($notification.siblings().filter(".hidden").length===$notification.siblings().length) {
-        $('#toast__container').hide();
-    }
-    
-    // if ($notification.siblings()===0) {
-    //     $('#toast__container').hide();
-    // }
-    $notification.addClass("hidden");
-});
+    function  cookieHandler(event){
+        event.stopPropagation();
+        $notification = $(this.parentNode.parentNode);
+        if ($notification.siblings().filter(".hidden").length === $notification.siblings().length) {
+            $('#toast__container').hide();
+        }
+
+        // if ($notification.siblings()===0) {
+        //     $('#toast__container').hide();
+        // }
+        $notification.addClass("hidden");
+    };
+})(jQuery);
