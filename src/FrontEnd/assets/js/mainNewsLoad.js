9d67332dd875efb0487fc598dce4000f6ce500af
eognianov <emilian_89@abv.bg>
2019-05-29 12:34:04 +0300

    loading animation over loadMoreNews btn

--- src/FrontEnd/assets/js/mainNewsLoad.js
+++ src/FrontEnd/assets/js/mainNewsLoad.js
@@ -18,20 +18,38 @@ $(() => {
     loadMainNews();
 
     moreNewsBtn.on('click', loadMainNews);
+
+    function Loading(btn) {
+       return btn.attr("disabled").then(btn.addClass("btnLoading"));
+    }
+
+    function Loaded(btn) {
+        return btn.removeAttr("disabled").then(btn.removeClass("btnLoading"));
+    }
+
+    function addNews(element) {
+                    let newNews = $('<li><article><header><h3></h3><time></time></header><a class="image"><img src="/images/pic08.jpg"></a></article></li>');
+                    newNews.find('h3').html(element.Title);
+                    newNews.find('time').html(element.CreatedOn);
+                    newNews.find('a.image').attr({ href: element.OriginalUrl, target: "_blank" });
+                    newNews.find('img').attr('src', element.ImageUrl);
+                    mainNewsList.append(newNews);
+    }
 			
     function loadMainNews() {
         let loadedNews = $('#mainNewsPosts > ul.posts li').length;
-        mainNewsService.gatLastMainNews(5,loadedNews).then((mainNews) => {
-            mainNews.forEach(element => {
-                let newNews = $('<li><article><header><h3></h3><time></time></header><a class="image"><img src="/images/pic08.jpg"></a></article></li>');
-                newNews.find('h3').html(element.Title);
-                newNews.find('time').html(element.CreatedOn);
-                newNews.find('a.image').attr({href: element.OriginalUrl, target: "_blank"});
-                newNews.find('img').attr('src', element.ImageUrl);
-                mainNewsList.append(newNews);
-            });
-        }).catch(function (err) {
-            console.log(err);
+        moreNewsBtn.addClass("btnLoading", {
+            complete: mainNewsService.gatLastMainNews(5, loadedNews).then((mainNews) => {
+                mainNews.map((element) => addNews(element), moreNewsBtn.removeClass('btnLoading'));
+            }).catch(function (err) {
+                console.log(err);
+            })
         });
     }
+
+    return {
+        moreNewsBtn,
+        Loading,
+        Loaded
+    };
 });
