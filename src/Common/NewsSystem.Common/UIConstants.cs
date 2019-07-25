519a30448dc6de6ebc9601d0404cdec8ddc73c49
eognianov <emilian_89@abv.bg>
2019-07-25 21:33:46 +0300

    create static class with constant for ui labels and text

---
+++ src/Common/NewsSystem.Common/UIConstants.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NewsSystem.Common
+{
+    public static class UIConstants
+    {
+        public const string HOME = "Начало";
+        public const string READ_MORE = "Прочети още";
+        public const string PREV_PAGE = "Предищна стр.";
+        public const string NEXT_PAGE = "Следваща стр.";
+
+    }
+}
+
+
