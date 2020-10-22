1bdb364abcf67380e7d4e4a9843a090da295a62b
eognianov <emilian_89@abv.bg>
2020-10-22 17:13:06 +0300

    new sources added

---
+++ src/Services/NewsSystem.Services.Sources/MainNews/NewsBntBgMainNewsProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace NewsSystem.Services.Sources.MainNews
+{
+    public class NewsBntBgMainNewsProvider : BaseMainNewsProvider
+    {
+        public override string BaseUrl { get; } = "https://news.bnt.bg";
+
+        public override RemoteMainNews GetMainNews()
+        {
+            var document = this.GetDocument(this.BaseUrl + "/bg");
+
+            var titleElement = document.QuerySelector(".top-img h1.home-news-title");
+            var title = titleElement.TextContent.Trim();
+
+            var urlElement = document.QuerySelector(".top-img .img-wrap a");
+            var url = urlElement.Attributes["href"].Value.Trim();
+
+
+            var imageElement = document.QuerySelector(".top-img .img-wrap img");
+            var imageUrl = imageElement?.Attributes["src"]?.Value?.Trim();
+
+
+            return new RemoteMainNews(title, url, imageUrl);
+        }
+    }
+}
